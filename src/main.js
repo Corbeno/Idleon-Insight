8b6e2367d5a6af9c1de649438a5ae81e3d9886bc
Corbeno <corbinkems@gmail.com>
2024-07-31 00:56:00 -0500

    latest

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ let ItemDetailRepo = getJSONFromFile(exportedPath + "repo/Item/ItemDetailRepo.js
 // ----------------------------------- REPO -----------------------------------
 // repo/Worlds/*/
 runParser(parseBribeRepo, exportedPath + "repo/Worlds/1/BribeRepo.json");
-runParser(parseBubbleRepo, exportedPath + "repo/Worlds/2/BubbleRepo.json");
+runParser(parseFeatherRepo, exportedPath + "repo/Worlds/1/FeatherRepo.json")
 
+runParser(parseBubbleRepo, exportedPath + "repo/Worlds/2/BubbleRepo.json");
 runParser(parsePostOfficeUpgradesRepo, exportedPath + "repo/Worlds/2/PostOfficeUpgradesRepo.json");
 runParser(parseSigilRepo, exportedPath + "repo/Worlds/2/SigilRepo.json");
 
@@ -109,6 +110,16 @@ function parseBribeRepo(keyValue){
         source: "Bribe",
         name: key,
         bonuses: [body.desc],
+    });
+}
+
+function parseFeatherRepo(keyValue){
+    let key = keyValue[0];
+    let body = keyValue[1];
+    output.push({
+        source: "Orion Feathers",
+        name: body.name,
+        bonuses: [body.bonus1, body.bonus2],
     });
 }
 
